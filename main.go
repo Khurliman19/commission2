1c72e87c4305fd3ec6dcf47e23881d97f4d86fe8
Student <hjolmurzaeva@gmail.com>
2026-04-19 20:48:23 +0500

    feat: добавлен ряд === в чек

--- main.go
+++ main.go
@@ -29,6 +29,7 @@ func printProduct(p Product) {
 	fmt.Printf("Цена: %d сум\n", p.Price)
 	fmt.Printf("Рассрочка: %d месяцев\n", p.Months)
 	fmt.Printf("В месяц: %d сум\n", monthlyPayment(p))
+	fmt.Println("=====================================")
 }
 
 func main() {
